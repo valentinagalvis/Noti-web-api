2aa8e02defd2529a063d2797b014435d4fdad636
ingmiguel2 <miguela.corream@konradlorenz.edu.co>
2025-10-22 21:32:01 -0500

    Se realizo el esquema del modelo de datos de noticiasSchema en la base de datos de mongo db

--- src/models/noticiaModel.js
+++ src/models/noticiaModel.js
@@ -1,1 +1,36 @@
 const mongoose = require("mongoose");
+const noticiaSchema = mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    contenido: {
+        type: String,
+        required: true
+    },
+    autor: {
+        type: String,
+        default: "Desconocido"
+    },
+    categoria: {
+        type: String,
+        enum: ["Arte","Moda","Cultura"],
+        requires: true
+    },
+    fuente: {
+        type: String,
+        default: "Interna"
+    },
+    revisado: {
+        type: Boolean,
+        default: false
+    },
+    fechaPublicacion: {
+        type: Date,
+        default: null
+    },
+    fechaRegistroBD: {
+        type: Date,
+        default: Date.now
+    }
+})
