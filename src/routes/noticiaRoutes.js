2232c7e82d6e84fcf3463c5e6d1e1062f271f806
ingmiguel2 <miguela.corream@konradlorenz.edu.co>
2025-10-22 21:51:40 -0500

    Se agrego el metodo HTTP post, para agregar noticias nuevas)

--- src/routes/noticiaRoutes.js
+++ src/routes/noticiaRoutes.js
@@ -2,3 +2,12 @@
 const express = require("express");
 const router = express.Router();
 const noticiaSchema = require("../models/noticiaModel");
+
+//Metodo Post, para agregar Noticias nuevas
+router.post("/noticia/add",(req,res) => {
+   const noticia = noticiaSchema(req.body);
+   noticia
+   .save()
+   .then((data) => res.json(data))
+   .catch((error) => res.json({message: error}));
+});
