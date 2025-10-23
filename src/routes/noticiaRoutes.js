1f94bef0e44c88e098e612f772b891d01c6fc523
ingmiguel2 <miguela.corream@konradlorenz.edu.co>
2025-10-22 21:43:27 -0500

    Se agrego un archivo llamado noticiaRoutes en la carpeta de routes en donde se realizo la importacion del framework express y el modelo de noticiaModel.js de la carpeta models

---
+++ src/routes/noticiaRoutes.js
@@ -0,0 +1,4 @@
+//Importaciones del framework express y noticiaModel de la carpeta models
+const express = require("express");
+const router = express.Router();
+const noticiaSchema = require("../models/noticiaModel");
