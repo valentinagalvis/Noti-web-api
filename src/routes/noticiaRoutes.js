ba6f4e1d2aea0a51854472d5954ec0b334250733
ingmiguel2 <miguela.corream@konradlorenz.edu.co>
2025-10-22 21:56:57 -0500

    Se agrego el metodo HTTP get, para traer todas las noticias de la base de datos

--- src/routes/noticiaRoutes.js
+++ src/routes/noticiaRoutes.js
@@ -3,11 +3,18 @@ const express = require("express");
 const router = express.Router();
 const noticiaSchema = require("../models/noticiaModel");
 
-//Metodo Post, para agregar Noticias nuevas
+//Metodo POST, para agregar Noticias nuevas
 router.post("/noticia/add",(req,res) => {
    const noticia = noticiaSchema(req.body);
    noticia
    .save()
    .then((data) => res.json(data))
    .catch((error) => res.json({message: error}));
+});
+
+//Metodo GET, para traer todos los datos de las noticias de la base de datos
+router.get("/noticia/all", (req,res) => {
+    noticiaSchema.find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({message: error}));
 });
